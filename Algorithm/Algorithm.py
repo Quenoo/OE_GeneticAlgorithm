af4ea77a09d8e1cef80b9d6973cd858a66c58f08
Quenoo <Miki10040@gmail.com>
2021-03-21 15:52:23 +0100

    Changed algorithm dependencies

--- Algorithm/Algorithm.py
+++ Algorithm/Algorithm.py
@@ -1,9 +1,14 @@
 from Algorithm.Population import *
+from Algorithm.Crossover import *
 from Algorithm.Function import booth_function
 
 
 class Algorithm:
+    def __init__(self):
+        self.Crossover = Crossover('one_point_cross', 0.6)
+
     def run(self):
+
         pop = Population(booth_function, 10, 2, -10, 10, 6)
 
         # dla kazdej eopki
@@ -11,4 +16,5 @@ class Algorithm:
         # zapisz % najlepszych (strategia elitarna)
         # wybierz gatunki do crossowania (selection)
         # krzyzowanie gatunków (cross)
+        pop = self.Crossover.cross(pop)
         # mutacja i/lub inversja
--- Algorithm/Crossover.py
+++ Algorithm/Crossover.py
@@ -2,9 +2,16 @@ import numpy as np
 
 
 class Crossover:
+    def __init__(self, decision, prob):
+        self.decision = decision
+        self.prob = prob
 
-    @staticmethod
-    def one_point_cross(pop, prob):
+    def cross(self, pop):
+        if self.decision == 'one_point_cross':
+            return self.one_point_cross(pop)
+        else: pass
+
+    def one_point_cross(self, pop):
         new_pop = []
         pop_size = pop.shape[0]
         if pop_size % 2 != 0:
@@ -13,7 +20,7 @@ class Crossover:
             end = pop.shape[0]
         for i in range(0, end, 2):
             roll = np.random.random()
-            if roll < prob:
+            if roll < self.prob:
                 cross_p = np.random.randint(1, pop.shape[1])
                 new_chrom_1 = np.concatenate([pop[i][:cross_p], pop[i + 1][cross_p:]])
                 new_chrom_2 = np.concatenate([pop[i + 1][:cross_p], pop[i][cross_p:]])
