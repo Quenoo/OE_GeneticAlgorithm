4e5e3ec894740ccc69a8dc8b7f745d06e96d8478
tzch96 <tzch96@gmail.com>
2021-03-21 18:14:07 +0100

    Remove other classes (to be re-done too)

--- Algorithm/Function.py
+++
@@ -1,4 +0,0 @@
-# Booth function - two-dimensional, usually evaluated on the square x_i in [-10, 10] for all i = 1, 2.
-# Global minimum: f(x*) = 0 at x* = (1, 3)
-def booth_function(x1, x2):
-    return (x1 + 2 * x2 - 7) ** 2 + (2 * x1 + x2 - 5) ** 2
